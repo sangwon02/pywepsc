b64f91b78e56fa4bc93a23ca0107e318f00ba3de
sangwon02 <sangwon020214@gmail.com>
2022-02-05 01:02:22 +0900

    웹스크래퍼 만드는중

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 
-indeed_resul = requests.get("https://search.indeed.jobs/main/jobs?keywords=python&sortBy=relevance&page=1")
+indeed_resul = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 
-print(indeed_resul)
+indeed_soup = BeautifulSoup(indeed_resul.text,"html.parser")
+
+pagination = indeed_soup.find("div", {"class":"pagination"})
+
+pages = pagination.find_all('a')
+spans = []
+
+for page in pages:
+    spans.append(page.find("span"))
+
+print(spans[:-1])
